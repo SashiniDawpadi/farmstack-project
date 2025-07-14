643d317a9e56b37348f0af6c75b63cd59853e965
SashiniDawpadi <sashinidawpadi@gmail.com>
2025-07-14 15:25:28 +0530

    created CRUD operations in backend

--- backend/database.py
+++ backend/database.py
@@ -3,8 +3,8 @@ from model import Todo
 # MogoDB driver
 import motor.motor_asyncio
 
-client = motor.motor_asyncio.AsyncIOMotorClient('mongodb:"//localhost:27017')
-database = client.TodoList
+client = motor.motor_asyncio.AsyncIOMotorClient('mongodb://localhost:27017/')
+database = client.ToDoList
 collection = database.todo
 
 
@@ -20,9 +20,20 @@ async def fetch_all_todos():
     return todos
 
 async def create_todo(todo):
-    document = todo
-    result = await collection.insert_one(document)
-    return result
+    result = await collection.insert_one(todo)
+    new_todo = await collection.find_one({"_id":result.inserted_id})
+    return {
+        "id":str(new_todo["_id"]),
+        "title":new_todo["title"],
+        "description":new_todo["description"],
+    }
 
 async def update_todo(title, desc):
-    await collection.update_one({"title":title},{"$set":{"description":desc}})
+    await collection.update_one({"title":title},{"$set":{
+        "description":desc}})
+    document = await collection.find_one({"title":title})
+    return document
+
+async def remove_todo(title):
+    await collection.delete_one({"title":title})
+    return True
